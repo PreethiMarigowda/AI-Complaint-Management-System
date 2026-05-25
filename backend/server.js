09c400ce835c0de2b8c2c757abdc6092bd85a634
Preethi Marigowda <preethi9270@gmail.com>
2026-05-25 18:34:05 +0530

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,19 @@ connectDB();
 const app = express();
 
 // MIDDLEWARE
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+
 app.use(express.urlencoded({
   extended: true,
 }));
+
 app.use("/uploads", express.static("uploads"));
 
 // ROUTES
